84d83977859c8581f4f83ccff07a32d288b836d9
maxtbuchholz <92826974+maxtbuchholz@users.noreply.github.com>
2024-04-07 16:03:16 -0500

    Initial Commit

---
+++ SleepNoise/SleepNoiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepNoiseApp.swift
+//  SleepNoise
+//
+//  Created by Max Buchholz on 4/7/24.
+//
+
+import SwiftUI
+
+@main
+struct SleepNoiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
